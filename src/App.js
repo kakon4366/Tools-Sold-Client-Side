8346aab3fe85091bb88b914c6539b18a411baa02
kakon4366 <kakonbarman4366@gmail.com>
2022-05-22 14:18:57 +0600

    Router setup and Header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Blog from "./Pages/Blog/Blog";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 import Header from "./Pages/Shared/Header/Header";
 
 function App() {
 	return (
 		<div>
 			<Header></Header>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/home" element={<Home />}></Route>
+				<Route path="/blog" element={<Blog />}></Route>
+				<Route path="/login" element={<Login />}></Route>
+				<Route path="/register" element={<Register />}></Route>
+			</Routes>
 		</div>
 	);
 }
