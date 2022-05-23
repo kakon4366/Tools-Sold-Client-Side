c74132f18907cd8a0f924cc0fea4ec6233395298
kakon4366 <kakonbarman4366@gmail.com>
2022-05-23 11:27:52 +0600

    Login form validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,26 @@
 import React from "react";
 import SocialLogin from "./SocialLogin";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm();
+
+	const onSubmit = (data, e) => {
+		e.preventDefault();
+		console.log(data);
+	};
+
 	return (
 		<section className="my-12">
 			<div className="container mx-auto">
 				<div className="flex justify-center items-center">
 					<div className="w-[400px]">
 						<form
+							onSubmit={handleSubmit(onSubmit)}
 							action=""
 							className="border bg-slate-100 p-5 rounded-md shadow-sm"
 						>
@@ -21,9 +34,28 @@ const Login = () => {
 									type="email"
 									placeholder="Email Address"
 									className="input input-bordered w-full"
+									{...register("email", {
+										required: {
+											value: true,
+											message: "Email Address is required.",
+										},
+										pattern: {
+											value: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+											message: "Provide a valid email",
+										},
+									})}
 								/>
 								<label className="label">
-									<span className="label-text-alt">Alt label</span>
+									{errors.email?.type === "required" && (
+										<span className="label-text-alt text-red-500">
+											{errors.email.message}
+										</span>
+									)}
+									{errors.email?.type === "pattern" && (
+										<span className="label-text-alt text-red-500">
+											{errors.email.message}
+										</span>
+									)}
 								</label>
 							</div>
 							<div className="form-control w-full">
@@ -34,9 +66,28 @@ const Login = () => {
 									type="password"
 									placeholder="Password"
 									className="input input-bordered w-full"
+									{...register("password", {
+										required: {
+											value: true,
+											message: "Password is required.",
+										},
+										minLength: {
+											value: 6,
+											message: "Password will be character and more",
+										},
+									})}
 								/>
 								<label className="label">
-									<span className="label-text-alt">Alt label</span>
+									{errors.password?.type === "required" && (
+										<span className="label-text-alt text-red-500">
+											{errors.password.message}
+										</span>
+									)}
+									{errors.password?.type === "minLength" && (
+										<span className="label-text-alt text-red-500">
+											{errors.password.message}
+										</span>
+									)}
 								</label>
 							</div>
 
