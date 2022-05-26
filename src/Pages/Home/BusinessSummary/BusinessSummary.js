46a004f2fba72b6ac4882f7b7eae6173d7adfda3
kakon4366 <kakonbarman4366@gmail.com>
2022-05-26 16:03:43 +0600

    Business Summary page complited

--- src/Pages/Home/BusinessSummary/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary/BusinessSummary.js
@@ -1,39 +1,57 @@
 import React from "react";
-import SectionTitle from "../../../Components/SectionTitle";
 import bgImg from "../../../Images/banner/tools-3.jpg";
+import useProducts from "../../../Hooks/useProducts";
+import Loading from "../../Shared/Loading/Loading";
+import { useQuery } from "react-query";
 
 const BusinessSummary = () => {
+	const [products, isLoading] = useProducts();
+
+	const { data: reviews, isLoading: reviewLoading } = useQuery("reviews", () =>
+		fetch("http://localhost:5000/all-review").then((res) => res.json())
+	);
+
+	const { data: orders, isLoading: orderLoading } = useQuery("allOrders", () =>
+		fetch("http://localhost:5000/all-order").then((res) => res.json())
+	);
+
+	if (isLoading || reviewLoading || orderLoading) {
+		return <Loading></Loading>;
+	}
+
 	return (
 		<section className="my-24">
 			<div className="">
-				<SectionTitle>Business Summary</SectionTitle>
-
 				<div
 					className="hero min-h-[500px] mt-12"
 					style={{ backgroundImage: `url(${bgImg})` }}
 				>
 					<div className="hero-overlay bg-opacity-60 bg-accent"></div>
 					<div className="hero-content text-center text-neutral-content">
-						<div className="md:w-3/4 w-sull text-white">
-							<h1 className="mb-5 sm:text-5xl text-3xl font-bold">
-								What is our Business?
+						<div className="w-full text-white">
+							<h1 className="mb-5 sm:text-4xl text-3xl font-bold">
+								Business Summary
 							</h1>
-							<p className="mb-5 text-slate-300">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Dolorem nostrum, porro possimus obcaecati quam
-								dignissimos nihil odit ipsa laboriosam ratione corporis?
-								Porro laborum laudantium facilis exercitationem dicta,
-								perspiciatis fugit sunt fugiat beatae deserunt quisquam
-								aliquam ab magni excepturi quia corporis dolore ipsum
-								facere ipsa incidunt accusamus pariatur molestias
-								blanditiis illo! Doloremque aperiam eius, maxime alias
-								ea adipisci voluptas laborum eos cupiditate sit quae
-								saepe. Accusantium maiores ex rem quibusdam saepe, natus
-								quaerat sit illum itaque veritatis officiis aut sed at
-								est totam possimus doloribus nisi voluptatem cupiditate
-								dolorum sunt autem ipsa quam. Quod ipsa voluptas
-								explicabo dolorum distinctio! Quam, obcaecati.
-							</p>
+							<div className="mx-auto">
+								<div class="grid grid-cols-1 md:grid-cols-3 rounded-lg bg-white text-black shadow">
+									<div class="stat place-items-center">
+										<div class="stat-title">Products</div>
+										<div class="stat-value">{products.length}</div>
+									</div>
+
+									<div class="stat place-items-center">
+										<div class="stat-title">Reviews</div>
+										<div class="stat-value text-secondary">
+											{reviews.length}
+										</div>
+									</div>
+
+									<div class="stat place-items-center">
+										<div class="stat-title">Total Orders</div>
+										<div class="stat-value">{orders.length}</div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
