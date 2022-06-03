8fed868a184d691a826a7a200a3ff644e5921b70
kakon4366 <kakonbarman4366@gmail.com>
2022-06-03 21:34:35 +0600

    email catch success

--- src/Pages/Home/Subscribe/Subscribe.js
+++ src/Pages/Home/Subscribe/Subscribe.js
@@ -1,18 +1,31 @@
 import React from "react";
 
 const Subscribe = () => {
+	const handleSubscribe = (e) => {
+		e.preventDefault();
+		const email = e.target.email.value;
+		console.log(email);
+	};
+
 	return (
 		<section className="bg-secondary py-12">
 			<div className="container mx-auto px-2">
-				<div className="text-center md:flex justify-center items-center">
-					<input
-						type="email"
-						placeholder="Your Email Address"
-						className="input input-bordered w-full md:max-w-sm"
-					/>
-					<button className="btn mt-4 md:mt-0 btn-accent">
-						Subscribe
-					</button>
+				<div>
+					<form
+						onSubmit={handleSubscribe}
+						action=""
+						className="text-center md:flex justify-center items-center"
+					>
+						<input
+							type="email"
+							name="email"
+							placeholder="Your Email Address"
+							className="input input-bordered w-full md:max-w-sm"
+						/>
+						<button type="submit" className="btn mt-4 md:mt-0 btn-accent">
+							Subscribe
+						</button>
+					</form>
 				</div>
 			</div>
 		</section>
